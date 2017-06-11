509f7a956f7e819ddcda37fd4d6a0bd1dee978f9
HX-Rd <huxleygit@gmail.com>
2017-06-11 23:38:29 +0100

    Added setup script

---
+++ scripts/centos/install-support-packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "Installing support packages in centos"
+sudo yum install git -y
+sudo yum install vim -y
+echo "Done installing support packages for centos"
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+echo "Setting up environment"
+echo "Checking distro version"
+distro=""
+centos=$(cat /etc/*-release | grep "centos")
+if [ ! -z "$centos" ]
+then
+    distro="centos"
+fi
+chmod -R +x ./scripts
+sudo ./scripts/$distro/install-support-packages.sh
+files=$(\ls -a .)
+backup_folder=$HOME/env-backup
+mkdir $HOME/env-backup
+blacklist=(
+    '.'
+    '..'
+    '.git'
+    'setup.sh'
+    'scripts'
+)
+for file in $files
+do
+    if [ -f $HOME/$file ] || [ -d $HOME/$file ]
+    then
+        if [[ ! " ${blacklist[@]} " =~ " ${file} " ]]
+        then
+            mv $HOME/$file $backup_folder/$file
+        fi
+    fi
+    if [[ ! " ${blacklist[@]} " =~ " ${file} " ]]
+    then
+        mv ./$file $HOME/$file
+    fi
+
+done
+echo "Done setting up environment"
