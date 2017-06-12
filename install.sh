fec25d31dcc5208bebf4290e338834b54336f036
HX-Rd <huxleygit@gmail.com>
2017-06-12 01:42:04 +0100

    Added ubuntu support

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 distro=""
 centos=$(cat /etc/*-release | grep "centos")
+ubuntu=$(cat /etc/*-release | grep "ubuntu")
 if [ ! -z "$centos" ]
 then
     distro="centos"
+fi
+if [ ! -z "$ubuntu" ]
+then
+    distro="ubuntu"
 fi
 
 if [ $distro == "centos" ]
 then
     sudo yum install git -y
     sudo yum install vim -y
+fi
+if [ $distro == "ubuntu" ]
+then
+    sudo apt-get update
+    sudo apt-get install git -y
+    sudo apt-get install vim -y
 fi
 mkdir $HOME/lx-pref
 git clone https://github.com/HX-Rd/lx-pref.git $HOME/lx-pref
